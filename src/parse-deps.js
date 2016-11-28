86292d0bb7d881b2855a09d7c282373fd2143fee
noppa <the.nobba@gmail.com>
2016-11-29 01:12:07 +0200

    Replace Jest with Karma

--- src/parse-deps.js
+++ src/parse-deps.js
@@ -1,4 +1,58 @@
 
-const foo = () => true;
+const parse = _func => {
+  const func = _func.toString(),
+    l = func.length;
 
-export {foo}
+  var argnames = [],
+    index = 0;
+
+  var state = {
+    inComment: false,
+    inArgDef: false,
+    inArgName: false,
+    inSignature: false,
+    openParens: 0
+  };
+
+  while(index++ < l) {
+    let char = func[index];
+
+    if (state.inComment && char === '*' && func[index+1] === '/') {
+      state.inComment = false;
+      index++;
+      continue;
+    }
+
+    if (!state.inSignature) {
+      if (!state.inComment && char === '(') {
+        state.inSignature = true;
+      } else {
+        continue;
+      }
+    } else if (!state.inComment && !state.inArgDef) {
+
+    }
+
+    switch (char) {
+
+      case '/':
+        if (func[index+1] === '*') {
+          state.inComment = true;
+          index++;
+        }
+        break;
+
+      case '(':
+        if (!state.inSignature) {
+          state.inSignature = true;
+        }
+        break;
+
+      case 
+
+    }
+  }
+
+
+
+};
