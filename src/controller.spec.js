4dc0b40c4a6f54952cf7bfaa0492872539b88d40
Oskari Noppa <the.nobba@gmail.com>
2016-12-06 04:52:53 +0200

    Add a failing test for the ControllerProvider

--- src/controller.spec.js
+++ src/controller.spec.js
@@ -2,30 +2,51 @@ import { ControllerProvider } from './controller';
 
 const testModuleName = 'hot-reload-demo';
 
-fdescribe('ControllerProvider', () => {
+describe('ControllerProvider', () => {
   describe('creating a controller through the provider', () => {
-    var ctrl;
+    const testCtrlName = 'UnitTestController',
+      someValue = {}; // Some value to provide for the controller
+
+    var $controller, $rootScope;
 
     beforeEach(function() {
+      // Register a simple controller to test
       new ControllerProvider(testModuleName)
-        .register('UnitTestCtrl', class TestCtrl {
-
+        .register(testCtrlName, class TestCtrl {
           constructor() {
-            this.testCtrlName = 'UnitTestCtrl';
+            this.testCtrlName = testCtrlName;
           }
+        });
 
+      // Register a controller with local bindings
+      new ControllerProvider(testModuleName)
+        .register(testCtrlName + 'WithLocalValue', class TestCtrl {
+          constructor(someValue) {
+            this.someValue = someValue;
+          }
         });
     });
 
     beforeEach(angular.mock.module(testModuleName));
 
-    beforeEach(inject(function($controller, $rootScope) {
-      ctrl = $controller('UnitTestCtrl', { $scope: $rootScope.$new() });
+    beforeEach(inject(function(_$controller_, _$rootScope_) {
+      $controller = _$controller_;
+      $rootScope = _$rootScope_;
     }));
 
-    it('should have been initialized correctly', function() {
-      expect(ctrl).toBeDefined();
-      expect(ctrl && ctrl.testCtrlName).toBe('UnitTestCtrl');
+    it('should initialize simple controller correctly', function() {
+      var ctrl = $controller(testCtrlName, {
+        $scope: $rootScope.$new(),
+      });
+      expect(ctrl && ctrl.testCtrlName).toBe(testCtrlName);
+    });
+
+    it('should initialize controller with locals correctly', function() {
+      var ctrl = $controller(testCtrlName + 'WithLocalValue', {
+        $scope: $rootScope.$new(),
+        someValue,
+      });
+      expect(ctrl.someValue).toBe(someValue);
     });
   });
 });
