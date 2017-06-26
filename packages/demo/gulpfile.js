fec024c19632fe80e13a532de9596e45f4402adb
Oskari Noppa <the.nobba@gmail.com>
2017-06-26 03:25:29 +0300

    [WIP] Testing out how things could work

--- packages/demo/gulpfile.js
+++ packages/demo/gulpfile.js
@@ -4,23 +4,40 @@ var browserSyncSpa = require('browser-sync-spa');
 var del = require('del');
 var concat = require('gulp-concat');
 var iife = require('gulp-iife');
+var path = require('path');
 
-gulp.task('serve', ['clean'], function() {
+gulp.task('serve', ['build'], function() {
     var bs = browserSync.create();
 
     bs.use(browserSyncSpa({}));
+    bs.use({
+        'plugin:name': 'reload-test',
+        'hooks': {
+            'client:js': (...args) =>
+                console.log('HOOK', args),
+        },
+    });
     bs.init({
         open: false,
         startPath: '/',
         server: {
-            baseDir: '.',
+            baseDir: __dirname,
             routes: {
-                '/': './dist',
+                '/': path.join(__dirname, 'dist'),
             },
             ghostMode: false,
         },
     });
 
+    gulp.watch([
+        './gulp-example/**/*.js',
+    ], function(event) {
+        console.log('update', Object.keys(event));
+        bs.reload(event.path);
+    });
+});
+
+gulp.task('build', ['clean'], function() {
     gulp.src([
         './node_modules/angular/angular.js',
         './gulp-example/app.module.js',
