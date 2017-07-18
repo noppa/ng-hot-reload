4cab00c11ed1cc98b248ee1cfd0fcaf311897a98
Oskari Noppa <the.nobba@gmail.com>
2017-07-19 02:16:06 +0300

    Use eslint to prettify code

--- packages/core/src/component.js
+++ packages/core/src/component.js
@@ -2,11 +2,10 @@ import angularProvider from './ng/angular';
 import privateKey from './ng/private-key';
 
 function componentProvider(moduleName) {
-    const
-        angular = angularProvider(),
-        { isFunction, isArray, forEach } = angular;
+  const angular = angularProvider(),
+    { isFunction, isArray, forEach } = angular;
 
-    /**
+  /**
      * Calls `this.directive` with "normalized" definition object.
      *
      * NOTE: This is almost an exact copy angular's own component function:
@@ -17,57 +16,60 @@ function componentProvider(moduleName) {
      * @param {Object} options Component definition object
      * @return {*} Whatever this.directive returns.
      */
-    function registerComponent(name, options) {
-        return this.directive(name, ['$injector', function($injector) {
-            const def = {
-                controller: options.controller || function() {},
-                controllerAs:
-                    identifierForController(options.controller)
-                    || options.controllerAs
-                    || '$ctrl',
-                template: makeInjectable(
-                    (options.template || options.templateUrl) ?
-                        options.template : ''),
-                templateUrl: makeInjectable(options.templateUrl),
-                transclude: options.transclude,
-                scope: {},
-                bindToController: options.bindings || {},
-                restrict: 'E',
-                require: options.require,
+  function registerComponent(name, options) {
+    return this.directive(name, [
+      '$injector',
+      function($injector) {
+        const def = {
+          controller: options.controller || function() {},
+          controllerAs:
+            identifierForController(options.controller) ||
+            options.controllerAs ||
+            '$ctrl',
+          template: makeInjectable(
+            options.template || options.templateUrl ? options.template : ''
+          ),
+          templateUrl: makeInjectable(options.templateUrl),
+          transclude: options.transclude,
+          scope: {},
+          bindToController: options.bindings || {},
+          restrict: 'E',
+          require: options.require,
+        };
+
+        forEach(options, (val, key) => {
+          if (privateKey(key)) {
+            def[key] = val;
+          }
+        });
+
+        function identifierForController(controller) {
+          // TODO: Not implemented
+          return;
+        }
+
+        function makeInjectable(fn) {
+          if (isFunction(fn) || isArray(fn)) {
+            return function($element, $attrs) {
+              return $injector.invoke(fn, this, {
+                $element,
+                $attrs,
+              });
             };
+          } else {
+            return fn;
+          }
+        }
 
-            forEach(options, (val, key) => {
-                if (privateKey(key)) {
-                    def[key] = val;
-                }
-            });
+        return def;
+      },
+    ]);
+  }
 
-            function identifierForController(controller) {
-                // TODO: Not implemented
-                return;
-            }
-
-            function makeInjectable(fn) {
-                if (isFunction(fn) || isArray(fn)) {
-                    return function($element, $attrs) {
-                        return $injector.invoke(fn, this, {
-                            $element,
-                            $attrs,
-                        });
-                    };
-                } else {
-                    return fn;
-                }
-            }
-
-            return def;
-        }]);
-    }
-
-    return {
-        create: registerComponent,
-        update: registerComponent,
-    };
-};
+  return {
+    create: registerComponent,
+    update: registerComponent,
+  };
+}
 
 export default componentProvider;
