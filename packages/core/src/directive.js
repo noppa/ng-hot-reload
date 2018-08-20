649095b014d636a381827899d309af9609adb8f5
Oskari Noppa <the.nobba@gmail.com>
2018-08-20 04:42:41 +0300

    Fix a type for TS checker

--- packages/core/src/directive.js
+++ packages/core/src/directive.js
@@ -22,7 +22,7 @@ function hasIsolateScope({ scope }) {
  * Factory for the functions that handle calls to angular.module(..).directive.
  *
  * @param {string} moduleName Name of the module
- * @return {Object<{ create: Function, update: Function }>} Api for creating
+ * @return {{ create: Function, update: Function }} Api for creating
  *        and updating directives.
  */
 const directiveProvider = moduleName => {
