de26b711382b7a77559c8d1f6e02a66fd6f54a14
Oskari Noppa <the.nobba@gmail.com>
2018-12-09 00:21:28 +0200

    Minor formatting change: tabs to spaces

--- packages/demo/server.js
+++ packages/demo/server.js
@@ -1,15 +1,15 @@
 var webpack = require('webpack'),
-	WebpackDevServer = require('webpack-dev-server'),
-	config = require('./webpack.config.js');
+  WebpackDevServer = require('webpack-dev-server'),
+  config = require('./webpack.config.js');
 
 new WebpackDevServer(webpack(config), {
-	publicPath: '/',
-	hot: true,
-	historyApiFallback: true,
+  publicPath: '/',
+  hot: true,
+  historyApiFallback: true,
 }).listen(8080, 'localhost', function(err, result) {
-	if (err) {
-		return console.log(err);
-	}
+  if (err) {
+    return console.log(err);
+  }
 
-	console.log('Listening localhost:8080');
+  console.log('Listening localhost:8080');
 });
