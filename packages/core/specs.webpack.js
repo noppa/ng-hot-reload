0d58171417fa1982f87da0b8b28df071e3fa9838
Oskari Noppa <the.nobba@gmail.com>
2020-03-15 04:12:05 +0200

    Fix lint errors

--- packages/core/specs.webpack.js
+++ packages/core/specs.webpack.js
@@ -1,9 +1,10 @@
+// @ts-ignore
 var testsContext = require.context('./src/', true, /\.spec\.js$/);
 testsContext.keys().forEach(function(path) {
-    try {
-        testsContext(path);
-    } catch (err) {
-        console.error('[ERROR] WITH SPEC FILE: ', path);
-        console.error(err);
-    }
+  try {
+    testsContext(path);
+  } catch (err) {
+    console.error('[ERROR] WITH SPEC FILE: ', path);
+    console.error(err);
+  }
 });
