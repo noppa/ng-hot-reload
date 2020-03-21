f497c264af772a4a92cca412509ecfe43456dd98
Oskari Noppa <the.nobba@gmail.com>
2020-03-21 23:58:14 +0200

    Fix crash in webpack loader
    Loader crashed when it was used without source maps.
    Also remove unneeded dependency to lodash.template. JS template strings
    work well enough here.

--- packages/loader/index.js
+++ packages/loader/index.js
@@ -1,39 +1,79 @@
-const template = require('lodash.template'),
-  fs = require('fs'),
-  path = require('path'),
-  loaderUtils = require('loader-utils'),
-  templatePath = path.join(__dirname, 'src', 'source.js.tpl'),
-  compiled = template(fs.readFileSync(templatePath, 'utf8')),
-  corePath = require.resolve('ng-hot-reload-core'),
-  sourceMap = require('source-map');
+const path = require('path');
+const loaderUtils = require('loader-utils');
+const corePath = require.resolve('ng-hot-reload-core');
+const sourceMap = require('source-map');
 
 // Tests that we don't modify our own library files, i.e. files that are in
 // ng-hot-reload/packages or one of the suffixed ng-hot-reload-* directories.
-var noTransform = /ng-hot-reload([\\/]packages[\\/]|-)(core|loader|standalone)/;
+const noTransform =
+  /ng-hot-reload([\\/]packages[\\/]|-)(core|loader|standalone)/;
+
+const codeWrapperTop = ({ corePath, forceRefresh, preserveState }) =>
+  `
+/* ng-hot-reload-loader */
+(function(__ngHotReloadLoaderAngularGlobal) {
+  var angular = module.hot ? (function() {
+    var loader = require(${ corePath });
+    return loader.decorateAngular({
+      angular: __ngHotReloadLoaderAngularGlobal,
+      forceRefresh: Boolean(${ forceRefresh }),
+      preserveState: Boolean(${ preserveState })
+    });
+  })() : __ngHotReloadLoaderAngularGlobal;
+
+  try {
+    (function() {/* ng-hot-reload-loader end*/
+
+`;
+
+const codeWrapperBottom = ({ requireAngular }) =>
+  `
+
+    })();/* ng-hot-reload-loader */
+  } finally {
+    (function() {
+      if (module.hot && angular.__ngHotReload$didRegisterProviders) {
+        module.hot.accept(function(err) {
+          if (err) {
+            console.error(err);
+          }
+        });
+      }
+    })();
+  }
+})(${ requireAngular });
+/* ng-hot-reload-loader end */
+`;
 
 async function transform(source, map) {
-  const callback = this.async();
   if (this.cacheable) {
     this.cacheable();
   }
   const options = loaderUtils.getOptions(this) || {};
 
   if (noTransform.test(this.resourcePath)) {
-    return callback(null, source, map);
+    return this.callback(null, source, map);
   }
-
-  const sourcePlaceholder = '<SOUCE_PLACEHOLDER>';
-  const result = compiled({
+  const topPart = codeWrapperTop({
     corePath: JSON.stringify(corePath),
-    source: sourcePlaceholder,
-    requireAngular: typeof options.requireAngular === 'string' ?
-      options.requireAngular :
-      '(require("angular"), angular)',
     // Boolean options that default to true.
     forceRefresh: options.forceRefresh !== false,
     preserveState: options.preserveState !== false,
   });
-  const [topPart, bottomPart] = result.split(sourcePlaceholder);
+  const bottomPart = codeWrapperBottom({
+    requireAngular: typeof options.requireAngular === 'string' ?
+      options.requireAngular :
+      '(require("angular"), angular)',
+  });
+
+  if (!map) {
+    // No source maps, just concat the source code together.
+    const result = topPart + source + bottomPart;
+    this.callback(null, result, map);
+    return;
+  }
+
+  const callback = this.async();
   await sourceMap.SourceMapConsumer.with(map, null, async (consumer) => {
     const node = sourceMap.SourceNode.fromStringWithSourceMap(source, consumer);
     node.prepend(topPart);
